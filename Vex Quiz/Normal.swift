79dc5c59efb161a8bc40e062e51fa2ae83f3be6a
Daniel Gutierrez <lexinad@gmail.com>
2017-04-16 23:24:32 -0700

    Finished Gameplay logic
    Basically, all these changes amount to me making it so the game is
    actually playable.

--- Vex Quiz/Normal.swift
+++ Vex Quiz/Normal.swift
@@ -22,6 +22,9 @@ class Normal: UIViewController {
     
     var questions = [Question]()
     var questionNumber = Int()
+    var answerNumber = Int()
+    var score: Int = 0
+    var originalQuestionCount = Int()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +33,7 @@ class Normal: UIViewController {
         let question1 = Question(question: "How much is that doggy in the window?", answers: ["$1", "$5", "$10", "$40"], answer: 3)
         let question2 = Question(question: "What is the best soda?", answers: ["Mist TWST", "Sprite", "7UP", "Root Beer"], answer: 0)
         questions = [question1, question2]
+        originalQuestionCount = questions.count
         
         selectQuestion()
     }
@@ -41,44 +45,67 @@ class Normal: UIViewController {
     
     func selectQuestion() {
         if questions.count > 0 {
-            questionNumber = 0
+            questionNumber = Int(arc4random_uniform(UInt32(questions.count)))
             questionLabel.text = questions[questionNumber].question
+            answerNumber = questions[questionNumber].answer
             
             for i in 0..<buttons.count {
                 buttons[i].setTitle(questions[questionNumber].answers[i], for: UIControlState.normal)
             }
             questions.remove(at: questionNumber)
+        } else {
+            self.performSegue(withIdentifier: "Gameover", sender: self)
         }
     }
     
     
     @IBAction func btn1Click(_ sender: UIButton) {
+        if answerNumber == 0 {
+            score += 1
+        }
+        selectQuestion()
+        
         
     }
     
     @IBAction func btn2Click(_ sender: UIButton) {
+        if answerNumber == 1 {
+            score += 1
+        }
+        selectQuestion()
         
     }
     
     
     @IBAction func btn3Click(_ sender: UIButton) {
+        if answerNumber == 2 {
+            score += 1
+        }
+        selectQuestion()
         
     }
     
     
     @IBAction func btn4Click(_ sender: UIButton) {
+        if answerNumber == 3 {
+            score += 1
+        }
+        selectQuestion()
         
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "Gameover" {
+            let destination = segue.destination as! EndScreen
+            destination.score = score
+            destination.questionCount = originalQuestionCount
+        }
     }
-    */
+ 
 
 }
